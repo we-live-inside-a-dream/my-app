89f7376e09ff13c03546a704d6bdab5f17192923
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-01-15 17:19:09 -0700

    created material ui calendar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// import { formatDistance, subDays } from 'date-fns'
+import React, { useState } from 'react'
+import 'date-fns'
+import { DatePicker, TimePicker, DateTimePicker } from '@material-ui/pickers';
+// import Grid from '@material-ui/core/Grid'
+// import DateFnsUtils from '@date-io/date-fns'
+// import{
+//   MuiPickersUtilsProvider,
+//   KeyboardTimePicker,
+//   KeyboardDatePicker
+// }from '@material-ui/pickers'
+// import AdapterDateFns from '@mui/lab/AdapterDateFns';
+// import LocalizationProvider from '@mui/lab/LocalizationProvider';
 
 function App() {
+  const [dateSelected, setDateSelected] = useState(new Date())
+
+  console.log("date selected",dateSelected)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <lable>date</lable>
+        <DatePicker value={dateSelected} onChange={setDateSelected}/>
+      </div>
+
+      <div>
+        <lable>Hour</lable>
+        <TimePicker value={dateSelected} onChange={setDateSelected}/>
+      </div>
+
+      <div>
+        <lable>Date and Time</lable>
+        <DateTimePicker value={dateSelected} onChange={setDateSelected}/>
+      </div>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns'
+import caLocale from 'date-fns/locale/ca'
+
 
 ReactDOM.render(
   <React.StrictMode>
+  <MuiPickersUtilsProvider utils ={DateFnsUtils}>
     <App />
+    </MuiPickersUtilsProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
